3c123f2d1a6ecc3c63137169997038052f1c293a
mediba-Kitada <kitada@mediba.jp>
2014-03-27 01:28:11 +0900

    add recipe for apache

---
+++ site-cookbooks/apache/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: apache
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "httpd" do
+  action :install
+  version "2.2.15-29.el6.centos"
+end
