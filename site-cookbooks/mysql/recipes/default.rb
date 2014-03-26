7de56540b1b58eba4f91a3d18a4e6ef5b843b2a0
mediba-Kitada <kitada@mediba.jp>
2014-03-27 01:29:31 +0900

    add recipe for mysql

---
+++ site-cookbooks/mysql/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: mysql
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "mysql-server" do
+  action :install
+  #version "5.1.67-1.el6_3"
+end
