e95bd9a5a87e868cae891e1a2afa7b089c7dbc27
mediba-Kitada <kitada@mediba.jp>
2014-03-27 01:27:34 +0900

    modify recipe for php5.5

--- site-cookbooks/php55/recipes/default.rb
+++ site-cookbooks/php55/recipes/default.rb
@@ -7,33 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 
-yum_repository "epel" do
-  description "eple"
-  mirrorlist "http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch"
-  gpgkey "http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6"
-  fastestmirror_enabled true
-  action :create
+package "php" do
+  action :install
+  options "--enablerepo=remi,remi-php55"
+  version "5.5.10-1.el6.remi.1"
 end
 
-yum_repository 'remi' do
-  description 'Les RPM de Remi - Repository'
-  baseurl 'http://rpms.famillecollet.com/enterprise/6/remi/x86_64/'
-  gpgkey 'http://rpms.famillecollet.com/RPM-GPG-KEY-remi'
-  fastestmirror_enabled true
-  action :create
-end
-
-yum_repository 'remi-php55' do
-  description 'Les RPM de remi de PHP 5.5'
-  baseurl 'http://rpms.famillecollet.com/enterprise/6/php55/$basearch/'
-  mirrorlist 'http://rpms.famillecollet.com/enterprise/6/php55/mirror'
-  gpgkey 'http://rpms.famillecollet.com/RPM-GPG-KEY-remi'
-end
-
-%w{php php-pear php-mbstring php-devel php-mysql php-gd}.each do |pkg|
+%w{php-pear php-mbstring php-devel php-mysql php-gd php-mysql}.each do |pkg|
   package pkg do
     options "--enablerepo=remi,remi-php55"
     action :install
-    #version "5.5.10"
   end
 end
