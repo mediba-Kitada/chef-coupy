7a66fc5dd9ca7b3ee57e814beb524a072f5c737a
mediba-kitada <kitada@mediba.jp>
2014-04-10 17:26:13 +0900

    change phps version

--- site-cookbooks/php55/recipes/default.rb
+++ site-cookbooks/php55/recipes/default.rb
@@ -7,13 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-package "php" do
-  action :install
-  options "--enablerepo=remi,remi-php55"
-  version "5.5.10-1.el6.remi.1"
-end
-
-%w{php-pear php-mbstring php-devel php-mysql php-gd php-mysql}.each do |pkg|
+%w{php php-pear php-mbstring php-devel php-mysql php-gd php-mysql}.each do |pkg|
   package pkg do
     options "--enablerepo=remi,remi-php55"
     action :install
